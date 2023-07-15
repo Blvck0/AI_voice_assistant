b0897d6339d47588181e9da4de2d53679d668a45
kaiser olajuwon <kaiserjaey@gmail.com>
2023-07-16 00:51:27 +0100

    created the wishMe(), and also imported the datetime module.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import datetime
 engine = pyttsx3.init('sapi5')
 
 voices = engine.getProperty('voices')
@@ -10,6 +11,20 @@ author = Blvck
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak(f"Good Morning {author}")
+    elif hour >= 12 and hour < 18:
+        speak(f"Good Afternoon {author}")
+    else:
+        speak(f"Good Evening {author}")
+
+    speak(f"Hello {author} I am Jarvis, Please tell me how may I help you?")
+        
+
+
 
 if __name__ == '__main__':
     speak(f'Hello {author}, I am Jarvis')
